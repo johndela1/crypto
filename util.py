548065d7ba1ea9bdbce05676a414eb140cdbfd81
John de la Garza <john@jjdev.com>
2016-08-12 21:36:00 -0700

    add hamming dist calculation

--- util.py
+++ util.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python
 
+from textwrap import wrap
+from itertools import chain, cycle
+
 
 def chunks(seq, size):
     return (seq[i:i+size] for i in range(0, len(seq), size))
 
 
-#def encode_16(seq):
-#    return ''.join([hex(ord(c))[2:] for c in seq])
+def encode_16(seq):
+    return ''.join([hex(c)[2:] for c in seq])
+
 
 def decode_16(seq):
     return bytes(''.join(
@@ -65,30 +69,58 @@ def foo(seq):
             continue
 
 
+def cracked(seq):
+    try:
+        return sorted(((e, str(e).count(' '))
+                       for e in foo(seq)), key=lambda x: -x[1])[0]
+    except IndexError:
+        return None, None
+
+def bar():
+    with open('input.txt') as f:
+        for l in f:
+            bs = bytes(l.strip(), 'ascii')
+            msg, count = cracked(bs)
+            if msg and count > 3:
+                print(msg)
+
+
+def xor_repeat(key, lines):
+    return (x ^ ord(y) for x, y in zip(
+        bytes(''.join(chain(*lines)), 'ascii'),
+        cycle(key)))
+
+
+def h_dist(s1, s2):
+    return sum(bin(int(e, 16)).count('1') for e in hex_iter(xor(s1, s2)))
+
+
 if __name__ == '__main__':
     base_16 = b'49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
     base_64 = b'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t'
     b1 =  b'1c0111001f010100061a024b53535009181c'
     b2 = b'686974207468652062756c6c277320657965'
     c = b'746865206b696420646f6e277420706c6179'
-    
+
     assert(encode_16_64(base_16) == base_64) 
     assert(xor(b1, b2) == c)
-    #plain = decode_16(base_16)
-    #print('plain', plain)
-    #print('b64', encode_64(plain))
-    encoded_hex = b'1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736'
+    # plain = decode_16(base_16)
+    # print('plain', plain)
+    # print('b64', encode_64(plain))
+    # encoded_hex = b'1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736'
 
-    print(
-        sorted(((e, str(e).count(' '))
-                for e in foo(encoded_hex)), key=lambda x: -x[1])[0])
+    # print(cracked(encoded_hex))
+    # print(bar())
+    s=["Burning 'em, if you ain't quick and nimble",
+       "I go crazy when I hear a cymbal"]
+    for l in wrap(encode_16(xor_repeat('ICE', s)), 74):
+        print(l)
 
+    # print(xor_repeat('ICE', xor_repeat('ICE', b'Hey now')))
 
+    # 0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272
+    # a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f
+    s1 = b'this is a test'
+    s2 = b'wokka wokka!!!'
 
-
-
-
-
-
-
-
+    print(h_dist(encode_16(s1), encode_16(s2)))
