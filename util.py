28f7a3290fa67cecc805c7c84c13b347940f9977
John de la Garza <john@jjdev.com>
2016-08-13 04:19:29 -0700

    refacor and cleanup

--- util.py
+++ util.py
@@ -17,10 +17,6 @@ def chunks2(seq, size):
         yield bytes(''.join(acc), 'ascii')
 
 
-def enc_16(seq):
-    return ''.join('%0x' % c for c in seq)
-
-
 def dec_16(seq):
     return bytes(''.join(
         chr(int(chunk, 16)) for chunk in chunks(seq, 2)), 'ascii')
@@ -42,38 +38,73 @@ def pad2(seq, align):
     return bytes(l)
 
 
+SYMS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
+
+
 def jex(chunk):
-    SYMS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
     x, y, z = chunk
     n = x << 16 | y << 8 | z
     n1 = n >> 18 & 63
     n2 = n >> 12 & 63
     n3 = n >> 6 & 63
-    n4 = n  & 63
-    return SYMS[n1]+SYMS[n2]+SYMS[n3]+SYMS[n4]
+    n4 = n & 63
+    return bytes(SYMS[n1]+SYMS[n2]+SYMS[n3]+SYMS[n4], 'ascii')
 
 
 def enc_64(seq):
-    return bytes(''.join(jex(chunk) for chunk in chunks(pad2(seq, 3), 3)), 'ascii')
+    acc = bytes('', 'ascii')
+    for chunk in chunks(pad2(seq, 3), 3):
+        acc += (jex(chunk))
+    return acc
+
+
+def dec_64(seq):
+    acc = []
+    for s1, s2, s3, s4 in chunks(pad2(seq,4), 4):
+        x1 = SYMS.index(chr(s1))
+        x2 = SYMS.index(chr(s2))
+        x3 = SYMS.index(chr(s3))
+        x4 = SYMS.index(chr(s4))
+        x = x1 << 18 | x2 << 12 | x3 << 6 | x4
+        r1 = x >> 16 & 255
+        r2 = x >> 8 & 255
+        r3 = x & 255
+        acc.append(r1)
+        acc.append(r2)
+        acc.append(r3)
+    return acc
 
 
 def enc_16_64(seq):
     return enc_64(dec_16(seq))
 
 
-def hex_iter(seq):
-    return chunks(seq, 2)
-
-
 def xor(s1, s2):
-    return bytes(''.join('%0x' % (int(x, 16) ^ int(y, 16))
-                          for x, y in zip(hex_iter(s1), hex_iter(s2))), 'ascii')
+    return bytes(x ^ y for x, y in zip(s1, s2))
+
+
+def xor_repeat(key, lines):
+    c = cycle(ord(k) for k in key)
+    return bytes(x ^ y for x, y in zip(lines, c))
+
+def h_dist(s1, s2):
+    return sum(bin(e).count('1') for e in xor(s1, s2))
+
+
+def key_length_guess(seq):
+    acc = []
+    for size in range(2, 10):
+        s1 = seq[:size]
+        s2 = seq[size:size*2]
+        dist = h_dist(s1, s2)
+        acc.append((dist, size))
+    return sorted(acc)[0][1]
 
 def foo(seq):
     length = len(seq)
     for i in range(256):
         try:
-            yield dec_16(xor(seq, ''.join(['%0x' % i] * length)))
+            yield xor(seq, [i] * length)
         except UnicodeEncodeError:
             continue
 
@@ -85,6 +116,7 @@ def cracked(seq):
     except IndexError:
         return None, None
 
+
 def bar():
     with open('input.txt') as f:
         for l in f:
@@ -94,42 +126,29 @@ def bar():
                 print(msg)
 
 
-def xor_repeat(key, lines):
-    return (x ^ ord(y) for x, y in zip(
-        bytes(''.join(chain(*lines)), 'ascii'),
-        cycle(key)))
-
-
-def h_dist(s1, s2):
-    return sum(bin(int(e, 16)).count('1') for e in hex_iter(xor(s1, s2)))
-
-
 if __name__ == '__main__':
-    base_16 = b'49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
-    base_64 = b'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t'
-    b1 =  b'1c0111001f010100061a024b53535009181c'
-    b2 = b'686974207468652062756c6c277320657965'
-    c = b'746865206b696420646f6e277420706c6179'
-
-    assert(enc_16_64(base_16) == base_64) 
-    assert(xor(b1, b2) == c)
-    # plain = dec_16(base_16)
-    # print('plain', plain)
-    # print('b64', en_64(plain))
-    # enc_hex = b'1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736'
-
-    # print(cracked(enc_hex))
-    # print(bar())
-    s=["Burning 'em, if you ain't quick and nimble",
-       "I go crazy when I hear a cymbal"]
-    for l in wrap(enc_16(xor_repeat('ICE', s)), 74):
-        print(l)
-
-    # print(xor_repeat('ICE', xor_repeat('ICE', b'Hey now')))
-
-    # 0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272
-    # a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f
     s1 = b'this is a test'
     s2 = b'wokka wokka!!!'
+    assert(h_dist(s1, s2) == 37)
+    base_16 = b'49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
+    base_64 = b'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t'
+    b1 =  bytes.fromhex('1c0111001f010100061a024b53535009181c')
+    b2 = bytes.fromhex('686974207468652062756c6c277320657965')
+    c = bytes.fromhex('746865206b696420646f6e277420706c6179')
+    assert(enc_16_64(base_16) == base_64) 
+    assert(xor(b1, b2) == c)
 
-    print(h_dist(enc_16(s1), enc_16(s2)))
+    lines = b"Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal"
+    ref = '0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f'
+    cypher_text = xor_repeat('ICE', lines)
+    assert(cypher_text.hex() == ref)
+    assert(xor_repeat('ICE', cypher_text) == lines)
+
+    #enc_hex = b'1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736'
+    enc_b64 = b'HUIfTQsPAh9PE048GmllH0kcDk4TAQsHThsBFkU2AB4BSWQgVB0dQzNTTmVS'
+    bts = dec_64(enc_b64)
+    #    print('decrypt', xor_repeat('ICE', cypher_text))
+    bts = xor_repeat('ICEMAN', b'Since we are all here working on the farm we will talk a lot.  This is a test of some new crypto system')
+
+    print('decrypt', xor_repeat('ICEMAN', bts))
+    print(key_length_guess(bts))
